2c6843d64c46f8f43c89037f2e6f684cad81420c
yaseenizhar <yaseenizhar0@gmail.com>
2023-02-21 17:54:42 +0500

    firebase setyp

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-<script>
-   const firebaseApp = firebase.initializeApp({ /* Firebase config */ });
-   const db = firebaseApp.firestore();
-   const auth = firebaseApp.auth();
-</script>
+// Setting up Firebase with our Website
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBOouELRj1EShwsag75jFpr7S9LrwLCew8",
+  authDomain: "authentication-cb806.firebaseapp.com",
+  projectId: "authentication-cb806",
+  storageBucket: "authentication-cb806.appspot.com",
+  messagingSenderId: "332191129643",
+  appId: "1:332191129643:web:fa9a459510f7f2f6738a6e",
+});
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+
+// Sign Up Function
+
+const signup = ()=>{
+
+    const email = document.getElementById("email").value;
+    const Password = document.getElementById("Password").value;
+    console.log(email, Password);
+
+    firebase.auth().createUserWithEmailAndPassword(email, Password)
+  .then((result) => {
+    // Signed in 
+    document.write("You are Signed UP");
+    console.log(result);
+    // ...
+  })
+  .catch((error) => {
+   console.log(error.code);
+   console.log(error.message);
+    // ..
+  });
+}
